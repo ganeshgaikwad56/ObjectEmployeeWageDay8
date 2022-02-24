480401f5882780215161009ca2e2d34fccdf8ce3
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-02-24 22:50:04 +0530

    Adding Mian class file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    namespace Oops1EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage progrsam");
+
+            CheckEmployee check = new CheckEmployee( 1 );
+            
+            check.EmployeePresentOrNot();
+        }
+    }
+}
