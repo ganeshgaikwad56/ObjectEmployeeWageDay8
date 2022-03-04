cb9a2835f49bd1ee2a270e6d71621099bc3e5f39
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-04 10:40:42 +0530

    Greeting message to Employee Wage Program

--- Program.cs
+++ Program.cs
@@ -6,14 +6,31 @@
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Welcome to Employee wage progrsam");
+             Console.WriteLine("Welcome to Employee wage program");
 
             //CheckEmployee check = new CheckEmployee( 1 );
 
             //check.EmployeePresentOrNot();
 
-            ClassDay8 day = new ClassDay8();
-            day.EmployeeWage();
+            //ClassDay8 day = new ClassDay8();
+            //day.EmployeeWage();
+
+            //CalEmpDailyWage wage=new CalEmpDailyWage();
+            //wage.CalculateWage();
+
+            //SwitchCase sw = new SwitchCase();
+            //sw.SwitchCaseCal();
+
+            //EmpWageForMultCom com = new EmpWageForMultCom();
+            //com.EmpWageForCompanies("DMart", 20, 20, 100 );
+            //com.EmpWageForCompanies("tata", 40, 30, 140);
+
+            //EmployeeWageForCompanys dMart = new EmployeeWageForCompanys("DMart", 20, 20,10);
+            //EmployeeWageForCompanys relience = new EmployeeWageForCompanys("Reliance", 22, 30, 20);
+            //dMart.ComputeEmpWage();
+            //Console.WriteLine(dMart.tostring());
+            //relience.ComputeEmpWage();
+            //Console.WriteLine(relience.ToString());
         }
     }
 }
